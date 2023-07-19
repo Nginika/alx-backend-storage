bb847dcde4ebd310ec7d465b76a74e99d06fd4e8
Nginika <nginikaugochukwu@gmail.com>
2023-07-19 21:55:40 +0000

    another trial at this

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-"""implementing an expiring web cache and tracker"""
-import requests
+"""implement a get_page function (prototype: def get_page(url: str) -> str:).
+The core of the function is very simple. It uses the requests module
+to obtain the HTML content of a particular URL and returns it."""
 import redis
-from typing import Dict
+import requests
 
-redis_client = redis.Redis()
+# Redis connection
+r = redis.Redis()
 
 
 def get_page(url: str) -> str:
     """ Check if the URL content is already cached"""
-    cached_content = redis_client.get(url)
+    cached_content = r.get(f"cached:{url}")
     if cached_content:
         # URL content found in cache, return it
         return cached_content.decode('utf-8')
@@ -17,15 +19,16 @@ def get_page(url: str) -> str:
     # If URL content is not in cache, fetch it
     response = requests.get(url)
 
-    # Track the number of times the URL was accessed
-    url_count_key = f"count:{url}"
-    redis_client.incr(url_count_key)
-
     # Cache the content with an expiration time of 10 seconds
-    redis_client.setex(url, 10, response.text)
+    r.setex(f"cached:{url}", 10, response.text)
+
+    # Increment the count for the URL access
+    r.incr(f"count:{url}")
 
     return response.text
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    url = 'http://slowwly.robertomurray.co.uk'
+    page_content = get_page(url)
+    print(page_content)
