d766e597cce1c4f739a58cc20b0327e8465fe030
Nginika <nginikaugochukwu@gmail.com>
2023-07-19 19:29:44 +0000

    task 0 with cache class init and store method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,19 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 """writing strings to redis"""
 import redis
+from typing import Union
+import uuid
 
 
 class Cache:
+    """Cache class"""
     def __init__(self):
         """store and instance of redis clientas a private variable"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """creates key id and stores data in redis"""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
