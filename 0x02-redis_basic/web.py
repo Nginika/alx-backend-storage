05d18916b8f99680f68bdd053d7c37ad1e030be1
Nginika <nginikaugochukwu@gmail.com>
2023-07-19 21:25:15 +0000

    second trial

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 """implementing an expiring web cache and tracker"""
-import redis
 import requests
-r = redis.Redis()
-count = 0
+import redis
+from typing import Dict
+
+redis_client = redis.Redis()
 
 
 def get_page(url: str) -> str:
-    """uses the requests module to obtain the HTML content of a
-    particular URL and returns it"""
-    r.set(f"cached:{url}", count)
-    req = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return req.text
+    """ Check if the URL content is already cached"""
+    cached_content = redis_client.get(url)
+    if cached_content:
+        # URL content found in cache, return it
+        return cached_content.decode('utf-8')
+
+    # If URL content is not in cache, fetch it
+    response = requests.get(url)
+
+    # Track the number of times the URL was accessed
+    url_count_key = f"count:{url}"
+    redis_client.incr(url_count_key)
+
+    # Cache the content with an expiration time of 10 seconds
+    redis_client.setex(url, 10, response.text)
+
+    return response.text
 
 
 if __name__ == "__main__":
