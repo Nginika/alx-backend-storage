a3413e013940f030d5d3b9ad0b671ff8f03998e1
Nginika <nginikaugochukwu@gmail.com>
2023-07-21 21:07:46 +0000

    Another test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,22 @@
 #!/usr/bin/env python3
-"""
-Implements an expiring web cache and tracker
-"""
-from typing import Callable
-from functools import wraps
+"""implementing an expiring web cache and tracker"""
 import redis
 import requests
+r = redis.Redis()
+#count = 0
 
-redis_client = redis.Redis()
-
-
-def url_count(method: Callable) -> Callable:
-    """counts how many times an url is accessed"""
-    @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(url)  # Remove unnecessary f-string
-        if cached:
-            return cached.decode('utf-8')
-        response = method(url)  # Fetch the page content
-        redis_client.setex(url, 10, response)  # Fix setex arguments
-        return response
-
-    return wrapper
-
-
-@url_count
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
+    """uses the requests module to obtain the HTML content of a
+    particular URL and returns it"""
+    # r.set(f"cached:{url}", count)
+
+    if r.get(f"count:{url}") :
+        r.incr(f"count:{url}")
+    else :
+        r.setex(f"count:{url}", 10, 1)
+
+    req = requests.get(url)
+    return req.text
 
 
 if __name__ == "__main__":
